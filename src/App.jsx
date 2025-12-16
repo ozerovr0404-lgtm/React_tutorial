3f2210e0d5938dbce7874dc01a7845eb12943c18
RomanCode <ozrv@inbox.ru>
2025-12-17 00:33:40 +0500

    Немного переделал компонент ournalist

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
 import './App.css'
 import React, {useState} from 'react';
-import Button from './components/Button/Button';
-import JournalItem from './components/JournalItem/JournalItem';
 import LeftPanel from './layout/LeftPanel/LeftPanel.jsx';
 import Header from './components/Header/header';
 import Body from './layout/Body/body';
-import CardButton from './components/CardButton/CardButton.jsx';
 import JournalList from './components/JournalList/journalList';
 import JournalAddButton from './components/JournalAddButton/journalAddButton';
 import JournalForm from './components/JournalForm/JournalForm.jsx';
 
 const INITIAL_DATA = [
-    {
-      id: 1,
-      title: 'Подготовка к обновлению курсов',
-      text: 'Горные походы открывают удивительные природные ландшафты',
-      date: new Date()
-    },
-    {
-      id: 2,
-      title: 'Поход в горы',
-      text: 'Думал, что очень много времени...',
-      date: new Date()
-    }
+    // {
+    //   id: 1,
+    //   title: 'Подготовка к обновлению курсов',
+    //   text: 'Горные походы открывают удивительные природные ландшафты',
+    //   date: new Date()
+    // },
+    // {
+    //   id: 2,
+    //   title: 'Поход в горы',
+    //   text: 'Думал, что очень много времени...',
+    //   date: new Date()
+    // }
   ];
 
 function App() {
@@ -33,17 +30,11 @@ function App() {
       text: item.text,
       title: item.title,
       date: item.data ? new Date(item, date) : new Date(),
-      id: Math.max(...oldItems.map(i => i.id)) + 1
+      id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
   }]);
 };
 
-const sortItems = (a, b) => {
-  if (a.date < b.date) {
-    return 1;
-  } else {
-    return -1;
-  }
-}
+
 
   return (
     <div className='app'>
@@ -51,17 +42,7 @@ const sortItems = (a, b) => {
       <LeftPanel>
         <Header/>
         <JournalAddButton/>
-        <JournalList>
-          {items.sort(sortItems).map(el => (
-            <CardButton key={el.id}>
-              <JournalItem
-                title={el.title}
-                text={el.text}
-                date={el.date}
-              />
-          </CardButton>
-          ))}
-        </JournalList>
+        <JournalList items={items}/>
       </LeftPanel>
 
       <Body>
