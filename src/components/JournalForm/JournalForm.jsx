3abd77454b0007e5e76a6a3af6460bc6c44f1db0
RomanCode <ozrv@inbox.ru>
2025-12-27 13:22:44 +0500

    Добавил валидацию полей в LournalForm.jsx

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -4,6 +4,12 @@ import Button from '../Button/Button';
 
 function JournalForm({ onSubmit }) {
 
+    const [formValidState, setFormValidState] = useState({
+        title: true,
+        post: true,
+        date: true
+    });
+
     const [inputData, setInputData] = useState('');
 
     const inputChange = (event) => {
@@ -14,17 +20,41 @@ function JournalForm({ onSubmit }) {
         e.preventDefault();
         const formData = new FormData(e.target);
         const formProps = Object.fromEntries(formData);
+
+        let isFormValid = true;
+
+        if (!formProps.title.trim().length) {
+            setFormValidState(state => ({...state, title: false}));
+            isFormValid = false;
+        } else {
+            setFormValidState(state => ({...state, title: true}));
+        }
+        if (!formProps.post.trim().length) {
+            setFormValidState(state => ({...state, post: false}));
+            isFormValid = false;
+        } else {
+            setFormValidState(state => ({...state, post: true}));
+        }
+        if (!formProps.date) {
+            setFormValidState(state => ({...state, date: false}));
+            isFormValid = false;
+        } else {
+            setFormValidState(state => ({...state, date: true}));
+        }
+        if (!isFormValid) {
+            return;
+        }
         onSubmit(formProps);
     };
 
     return (
         <>
             <form className='journal-form' onSubmit={addJournalItem}>
-                <input type='text' name='title'/>
-                <input type='date' name='date'/>
-                <input type='text' name="tag" value={inputData} onChange={inputChange}/>
-                <textarea name="text" id="" cols="30" rows="10"></textarea>
-                <Button text="Сохранить" onClick={() => { console.log('Нажали');}} />
+                <input type='text' name='title' style={{ border: formValidState.title ? undefined : '1px solid red' }}/>
+                <input type='date' name='date' style={{ border: formValidState.date ? undefined : '1px solid red' }}/>
+                <input type='text' name="tag" value={inputData} onChange={inputChange} />
+                <textarea name="post" id="" cols="30" rows="10" style={{ border: formValidState.post ? undefined : '1px solid red' }}></textarea>
+                <Button type="submit" text="Сохранить" onClick={() => { console.log('Нажали');}} />
             </form>
         </>
     )
